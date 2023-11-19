ae9f0be2ed478e9b337434c02488d0c53acb78e5
ngrayzin <rayzin147@gmail.com>
2023-11-19 22:23:17 +0800

    updated scripts

--- init/03.sql
+++ init/03.sql
@@ -1,15 +1,15 @@
 USE mysql;
 
 -- Create the user and grant privileges
-CREATE USER 'mysql'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
 
 -- Grant all privileges
-GRANT ALL PRIVILEGES ON *.* TO 'mysql'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost';
 
-GRANT ALL PRIVILEGES ON *.* TO 'mysql'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'%';
 
 -- Grant ability to grant privileges
-GRANT GRANT OPTION ON *.* TO 'mysql'@'localhost';
+GRANT GRANT OPTION ON *.* TO 'user'@'localhost';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
